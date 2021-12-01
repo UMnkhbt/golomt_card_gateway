464cb375d2b6c9f771be2c3cf78ed6f2c87339dc
BumanIT <bumanit@Enkhjargals-iMac.local>
2021-12-01 14:20:22 +0800

    update url

--- index.py
+++ index.py
@@ -11,8 +11,8 @@ import requests, time, re, json, xmltodict
 
 app = Flask(__name__)
 
-loginUser = "Login"
-loginPass = "Pass"
+loginUser = "login"
+loginPass = "password"
 loginToken = "4DxHC7Ulh1734lbXyX6cMy4hibJMDYLbkOeo65ez+zs=$xih6efbMqNZ5Dy5PqC4N5FFfVizXCjLPvYkqwNPnCIo="
 mainURL = "https://uatvpn.golomtbank.com/cardpro/service"
 
