8811ea7e64ba719cf49758fba0d6d3fce8a69d44
BumanIT <bumanit@Enkhjargals-iMac.local>
2021-12-01 13:33:08 +0800

    update url

--- index.py
+++ index.py
@@ -101,7 +101,7 @@ def purchase_request():
   xmldata = xmldata.replace(' ', '')
   print(xmldata.strip())
   print("SENT")
-  vpnresult = send_request_to_vpn(loginToken, "", mainURL , "<Document><Header><Password>Pass</Password><TrxnType>Purchase</TrxnType><LoginId>Login</LoginId><MsgId>20200831145233</MsgId></Header><PosTxn><F42>000000000043752</F42><MsgType>0200</MsgType><F11>000002</F11><F22>0021</F22><F25>00</F25><F24>0003</F24><F35>9496100477947550d22095011632333200000</F35><F41>19190000</F41><F4>000000007000</F4><F3>000000</F3><F62>000009</F62></PosTxn></Document>", "POST")
+  vpnresult = send_request_to_vpn(loginToken, "", mainURL , xmldata.strip(), "POST")
   print("RECEIVE")
   print(vpnresult)
   print(vpnresult.status_code)
@@ -388,7 +388,11 @@ def send_request_to_vpn(proxy_auth_token, proxy_cert_data, url_string, params, r
   #     "Accept": "application/json",
   #     "Connection": "keep-alive",
   # }
-  headers = {}
+  headers = {
+    "Content-Type": "application/xml",
+    "Accept": "application/xml",
+    "Connection": "keep-alive",
+  }
   if proxy_auth_token:
       headers["Authentication"] = proxy_auth_token
 
